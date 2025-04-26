e25cd0ff19d8b8297d350ec53c389777ad695718
Arman Nikoyan <armannikoyan@Armans-MacBook-Air.local>
2025-04-26 13:17:28 +0400

    change: icons for e/w/i/h

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -20,7 +20,6 @@ return {
 			local config = {
 				options = {
 					icons_enabled = true,
-					theme = require('rusty').lualine,
 					component_separators = '',
 					section_separators = '',
 					disabled_filetypes = {
@@ -58,7 +57,7 @@ return {
 						},
 						{
 							'diagnostics',
-							symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+							symbols = { error = '󰅘 ', warn = '󰳤 ', info = '󰰃 ', hint = '󰏭 ' },
 							separator = { right = '' },
 						},
 					},
