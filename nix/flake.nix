c679d486482ed29e3269ad8a0ffd73d0306f9903
Arman Nikoyan <armannikoyan04@icloud.com>
2025-09-01 13:18:30 +0400

    added: gcc

--- nix/flake.nix
+++ nix/flake.nix
@@ -33,6 +33,7 @@
           pkgs.zulu
           pkgs.fvm
           pkgs.cocoapods
+          pkgs.gcc
         ];
 
       homebrew =
@@ -52,24 +53,24 @@
         ];
 
       system.activationScripts.applications.text = let
-	      env = pkgs.buildEnv {
-		      name = "system-applications";
-		      paths = config.environment.systemPackages;
-		      pathsToLink = "/Applications";
-	      };
+       env = pkgs.buildEnv {
+        name = "system-applications";
+        paths = config.environment.systemPackages;
+        pathsToLink = "/Applications";
+       };
       in
         pkgs.lib.mkForce ''
         # Set up applications.
-	      echo "setting up /Applications..." >&2
-	      rm -rf /Applications/Nix\ Apps
-	      mkdir -p /Applications/Nix\ Apps
-	      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-	      while read -r src; do
-		      app_name=$(basename "$src")
-			      echo "copying $src" >&2
-			      ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/$app_name"
-			      done
-			      '';
+       echo "setting up /Applications..." >&2
+       rm -rf /Applications/Nix\ Apps
+       mkdir -p /Applications/Nix\ Apps
+       find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+       while read -r src; do
+        app_name=$(basename "$src")
+         echo "copying $src" >&2
+         ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/$app_name"
+         done
+         '';
 
       system.primaryUser = "armannikoyan";
 
