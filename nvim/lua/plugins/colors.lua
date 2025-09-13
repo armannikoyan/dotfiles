63f9aa5bec00a4dd4dd587955a505b3463f74f78
Arman Nikoyan <armannikoyan04@icloud.com>
2025-09-13 15:31:26 +0400

    quick-fix: not necessary colorings deleted

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -7,12 +7,8 @@ return {
     italic_comments = true,
     underline_current_line = true,
   },
+
   config = function(_, opts)
     require("rusty").setup(opts)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
   end,
-
 }
